3ebe6fba82d50ea9ea8b616d22b7bb02237e4bc6
Chenyu Shi <HOLYGEEK17@gmail.com>
2019-05-01 17:37:56 -0400

    Replacement for Google Charts usage

--- src/main/java/com/warrenstrange/googleauth/GoogleAuthenticatorQRGenerator.java
+++ src/main/java/com/warrenstrange/googleauth/GoogleAuthenticatorQRGenerator.java
@@ -1,37 +1,6 @@
-/*
- * Copyright (c) 2014-2017 Enrico M. Crisostomo
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice, this
- *     list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *
- *   * Neither the name of the author nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-package com.warrenstrange.googleauth;
+package com.warrenstrange.googleauth; 
 
 import org.apache.http.client.utils.URIBuilder;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -47,7 +16,8 @@ public final class GoogleAuthenticatorQRGenerator
      * The format string to generate the Google Chart HTTP API call.
      */
     private static final String TOTP_URI_FORMAT =
-            "https://api.qrserver.com/v1/create-qr-code/?data=%s&size=200x200&ecc=M&margin=0";
+//    		"https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=%s";
+            "https://api.qrserver.com/v1/create-qr-code/?data=%s&size=200x200&ecc=M&margin=10";
 
     /**
      * This method wraps the invocation of <code>URLEncoder##encode</code>
@@ -83,7 +53,7 @@ public final class GoogleAuthenticatorQRGenerator
      * Neither issuer nor account name may themselves contain a colon.
      * Represented in ABNF according to RFC 5234:
      * <p/>
-     * label = accountname / issuer (“:” / “%3A”) *”%20” accountname
+     * label = accountname / issuer 
      *
      * @see <a href="https://code.google.com/p/google-authenticator/wiki/KeyUriFormat">Google Authenticator - KeyUriFormat</a>
      */
